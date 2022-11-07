a6b552646b53e758ad4970755b9995640ac39efe
Klebert-01 <arvid.berkert@gmail.com>
2022-11-07 22:13:12 +0100

    0.1 nya branchen

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ÄNDRAD");
+Console.WriteLine("ÄNDRAD FRÅN ANNAN BRANCH");
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i);
+}
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Första edit");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Första edit");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("SISTA EDIT");
